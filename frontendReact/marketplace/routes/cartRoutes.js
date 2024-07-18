96949d0da54d0bf0e98d94d0206f28fd34c6ab34
AyushGupta11442 <ayushgupta11442@gmail.com>
2024-07-18 17:13:51 +0530

    done

---
+++ frontendReact/marketplace/routes/cartRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const middleware = require('../utilities/middleware');
+const cartController = require('../controllers/cartController')
+
+router.post('/add_to_cart', middleware, cartController.addtocart);
+
+// routes aded
+router.get('/fetch_cart',middleware,cartController.fetchcart)
+
+router.post('/remove_from_cart',middleware,cartController.removeFromCart)
+
+router.post('/change_quantity',middleware , cartController.changeQuantity )
+
+module.exports = router;
